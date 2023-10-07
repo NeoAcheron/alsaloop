be4d63440d82487c335da579fc86bb77d65a86a5
NeoAcheron <henkbezuidenhout@gmail.com>
2023-10-07 21:17:28 +0200

    Changed the data detection to look at the audio sample length, which only returns a value if the audio device is active on the host

--- alsaloop.py
+++ alsaloop.py
@@ -39,30 +39,39 @@ SAMPLE_MAXVAL = 32768
 CHANNELS = 2
 # Sample rate in samples per second
 SAMPLE_RATE = 48000
-PERIOD_SIZE = 2048
+PERIOD_SIZE = 1024
 # The duration of a measurement interval (after which the thresholds will be checked) in seconds.
-SAMPLE_SECONDS_BEFORE_CHECK = 1
+SAMPLE_SECONDS_BEFORE_CHECK = 0.15
 # The number of samples before each check
 SAMPLE_COUNT_BEFORE_CHECK = int((SAMPLE_RATE / CHANNELS) * SAMPLE_SECONDS_BEFORE_CHECK)
 # The time during which the input threshold hasn't been reached, before output is stopped.
 # This is useful for preventing the output device from turning off and on when there is a short silence in the input.
-SAMPLE_SECONDS_BEFORE_TURN_OFF = 15
+SAMPLE_SECONDS_BEFORE_TURN_OFF = 300
 # The number of checks which have to fail before audio is turned off.
-CHECK_NUMBER_BEFORE_TURN_OFF = int(SAMPLE_SECONDS_BEFORE_TURN_OFF / SAMPLE_COUNT_BEFORE_CHECK)
+CHECK_NUMBER_BEFORE_TURN_OFF = 20
 
+def open_mixer(output=False):
+    input_mixer = alsaaudio.Mixer(control='PCM', device="hw:CARD=UAC2Gadget")
+
+    if output:
+        output_mixer = alsaaudio.Mixer(control='DSPVolume', device="hw:CARD=sndrpihifiberry") 
+        return input_mixer, output_mixer
+
+    else:
+        return input_mixer
 
 def open_sound(output=False):
     input_device = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK, device="hw:CARD=UAC2Gadget,DEV=0")
     input_device.setchannels(CHANNELS)
     input_device.setrate(SAMPLE_RATE)
-    input_device.setformat(alsaaudio.PCM_FORMAT_S32_LE)
+    input_device.setformat(alsaaudio.PCM_FORMAT_S16_LE)
     input_device.setperiodsize(PERIOD_SIZE)
 
     if output:
         output_device = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK, alsaaudio.PCM_NONBLOCK, device=device)
         output_device.setchannels(CHANNELS)
         output_device.setrate(SAMPLE_RATE)
-        output_device.setformat(alsaaudio.PCM_FORMAT_S32_LE)
+        output_device.setformat(alsaaudio.PCM_FORMAT_S16_LE)
         return input_device, output_device
 
     else:
@@ -81,29 +90,17 @@ def stop_playback(_signalNumber, _frame):
 
 if __name__ == '__main__':
 
-    start_db_threshold = 0
-    stop_db_threshold = 0
-    try:
-        start_db_threshold = float(sys.argv[1])
-        if start_db_threshold > 0:
-            start_db_threshold = -start_db_threshold
-
-        # Define the stop threshold. This prevents hysteresis when the volume fluctuates just around the threshold.
-        stop_db_threshold = start_db_threshold - 3
-
-        print("using alsaloop with input level detection {:.1f} to start, {:.1f} to stop"
-              .format(start_db_threshold, stop_db_threshold))
-    except:
-        print("using alsaloop without input level detection")
-
     device = 'default'
     input_device = open_sound(output=False)
     output_device = None
     finished = False
+    input_mixer = None
+    output_mixer = None
+
+    input_volume = 0
+    output_volume = 0
 
     samples = 0
-    sample_sum = 0
-    max_sample = 0
     status = "-"
     rms_volume = 0
     input_detected = False
@@ -137,58 +134,48 @@ if __name__ == '__main__':
                 (sample_l, sample_r) = (0, 0)
 
             offset += 8
-            samples += 2
-            # Calculate the sum of all samples squared, used to determine rms later.
-            sample_sum += sample_l * sample_l + sample_r * sample_r
-            # Determine the max value of all samples
-            max_sample = max(max_sample, abs(sample_l), abs(sample_r))
 
-        if samples >= SAMPLE_COUNT_BEFORE_CHECK:
-            # Calculate RMS
-            rms_volume = sqrt(sample_sum / samples)
+        if data_length > 0:
+            samples += 1
+            input_detected = True
+            status = "p"
+        else:
+            input_detected = False
+            status = "-"
 
-            # Determine which threshold value to use
-            if output_stopped:
-                threshold = start_db_threshold
-            else:
-                threshold = stop_db_threshold
+        if output_stopped and input_detected:
+            del input_device
+            logging.info("Input signal detected, pausing other players")
+            os.system("/opt/hifiberry/bin/pause-all alsaloop")
+            (input_device, output_device) = open_sound(output=True)
+            (input_mixer, output_mixer) = open_mixer(output=True)
+            output_stopped = False
 
-            # Check if the threshold has been exceeded
-            if start_db_threshold == 0 or decibel(max_sample) > threshold:
-                input_detected = True
-                status = "P"
-            else:
-                input_detected = False
-                status = "-"
-
-            print("{} {:.1f} {:.1f}".format(status, decibel(rms_volume), decibel(max_sample)), flush=True)
-
-            sample_sum = 0
-            samples = 0
-            max_sample = 0
-
-            if output_stopped and input_detected:
+        elif not output_stopped and not input_detected:
+            count_playback_threshold_not_met += 1
+            # logging.info(f"No input signal for {count_playback_threshold_not_met} intervals")
+            if count_playback_threshold_not_met > CHECK_NUMBER_BEFORE_TURN_OFF:
                 del input_device
-                logging.info("Input signal detected, pausing other players")
-                os.system("/opt/hifiberry/bin/pause-all alsaloop")
-                (input_device, output_device) = open_sound(output=True)
-                output_stopped = False
+                input_mixer = None
+                output_device = None
+                output_mixer = None
+                logging.info("Input signal lost, stopping playback")
+                input_device = open_sound(output=False)
+                output_stopped = True
                 continue
+	
 
-            elif not output_stopped and not input_detected:
-                count_playback_threshold_not_met += 1
-                logging.info(f"No input signal for {count_playback_threshold_not_met} intervals")
-                if count_playback_threshold_not_met > CHECK_NUMBER_BEFORE_TURN_OFF:
-                    del input_device
-                    output_device = None
-                    logging.info("Input signal lost, stopping playback")
-                    input_device = open_sound(output=False)
-                    output_stopped = True
-                    continue
+        if input_detected and not output_stopped:
+            if samples > CHECK_NUMBER_BEFORE_TURN_OFF:
+                # Sync the volume        
+                input_mixer = open_mixer(output=False)
+                input_volume = input_mixer.getvolume(alsaaudio.PCM_CAPTURE)[0]
+                samples = 0
+                if input_volume != output_volume:
+                    output_mixer.setvolume(input_volume)
+                    output_volume = input_volume
 
-            if input_detected:
-                # Reset counter when input detected
-                count_playback_threshold_not_met = 0
+            count_playback_threshold_not_met = 0
 
-        if not output_stopped:
             output_device.write(data)
+
